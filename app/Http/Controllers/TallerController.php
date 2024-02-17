ce6e89747584d5d3851036723ab86d0ca52c0a4b
Alejo <b_alejandro3010@outlook.com>
2024-02-16 23:39:20 -0500

    primera entrega

---
+++ app/Http/Controllers/TallerController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TallerController extends Controller
+{
+    public function verificarPrimo($number)
+    {
+        $limit = sqrt($number);
+        $i = 3;
+        $message = "SÍ es un número primo";
+        if ($number % 2 == 0) {
+            $message = "NO es un número primo";
+        } else {
+            while ($limit >= $i) {
+                if ($number % $i == 0) {
+                    $message = "NO es un número primo";
+                }
+                $i = $i + 2;
+            }
+        }
+        return "{$number} {$message}";
+    }
+
+    public function verificarAmigos($number1, $number2){
+        $ob = new Operador();
+        $divisores_number1 = $ob->obtenerDivisores($number1);
+        $suma_divisores_number1 = $ob->sumar($divisores_number1);
+
+        $divisores_number2 = $ob->obtenerDivisores($number2);
+        $suma_divisores_number2 = $ob->sumar($divisores_number2);
+        if($suma_divisores_number1 == $number2 && $suma_divisores_number2 == $number1){
+            $message = "SÍ son";
+        }else{
+            $message = "NO son";
+        }
+        return $message;
+    }
+}
+
+class Operador
+{
+    function obtenerDivisores($numero)
+    {
+        $divisores = array();
+        for ($i = 1; $i < $numero; $i++) {
+            if ($numero % $i == 0) {
+                $divisores[] = $i;
+            }
+        }
+        return $divisores;
+    }
+
+    function sumar($lista)
+    {
+        return array_sum($lista);
+    }
+}
